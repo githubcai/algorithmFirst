917d9211c197ee28cf66903d152105d7808ed2ea
caixukun <2468085704@qq.com>
2016-07-31 23:48:20 +0800

    m

---
+++ 6/6.cpp
@@ -0,0 +1,13 @@
+#include <cstdio>
+#include <cstring>
+
+const int MAXD = 20;
+int s[1 << MAXD];
+
+int main(){
+    int D, I;
+    while(scanf("%d%d", &D, &I) == 2){
+        memset(s, 0);
+    }
+    return 0;
+}
